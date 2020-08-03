d111865c08b70c081eb7b32843fa17f437f396b1
antoine.labalette <antoine.labalette@telecom-sudparis.eu>
2020-08-03 21:58:32 +0200

    [a] prendre, stocker et récupérer une photo

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -5,8 +5,10 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
          IonCol, IonImg, IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -15,8 +17,17 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.base64 ?? photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
-            <IonFabButton action={()=>takePhoto()}>
+            <IonFabButton onClick={() => takePhoto()}>
                 <IonIcon icon={camera}></IonIcon>
             </IonFabButton>
         </IonFab>
